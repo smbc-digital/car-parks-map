7409c6950f2a5f1bed6ae9a84a2556ee014cee81
Daniel Kinder <daniel.kinder@stockport.gov.uk>
2021-01-21 15:42:52 +0000

    Layers, Popups, Token

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,5 +1,5 @@
-import { developablesitesPopup, permissionedPopup, notassessedPopup, tclaPopup} from './Popups' //devsitesPopup, notdevsitesPopup}
-import { developablesitesStyle, tcwmdcStyle, greenbeltStyle, floodzonesStyle, allsubmittedStyle, permissionedStyle, notassessedStyle, tclaStyle} from './Styles'
+import { carparksPopup} from './Popups' //devsitesPopup, notdevsitesPopup}
+import { carparksStyle} from './Styles'
 
 const Configuration = {
     Map: {
@@ -14,109 +14,19 @@ const Configuration = {
     },
     DynamicData: 
     [
-        //{
-          //  key: 'Tree Preservation Orders',
-            //url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tpo_merged&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            //layerOptions: {
-            //    onEachFeature: tpoPopup,
-            //    maxZoom: 2,
-            //    style: tpoStyle
-            //},
-            //displayOverlay: true,
-            //visibleByDefault: true
-        //},
-
         {
-            key: 'Green Belt',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:green_belt_os&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Car Parks',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:car_parks_points&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
             layerOptions: {
+                onEachFeature: carparksPopup,
                 maxZoom: 2,
-                style: greenbeltStyle
+                style: carparksStyle,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker(latlng)
+                },
             },
             displayOverlay: true,
             visibleByDefault: true
-        },
-        
-        {
-            key: 'Flood Risk Zones',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=flooding:flood_zones&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                maxZoom: 2,
-                style: floodzonesStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'Town Centre Living Area',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_tcla&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: tclaPopup,    
-                maxZoom: 2,
-                style: tclaStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },       
-        
-        {
-            key: 'Town Centre West Mayoral Development Corporation Area',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:town_centre_west_mdc&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                maxZoom: 2,
-                style: tcwmdcStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'Sites with plannning permission for housing',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:schema2020_resi_permissioned&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: permissionedPopup,
-                maxZoom: 2,
-                style: permissionedStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'All sites considered in SHLAA 2020',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_allsites&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                maxZoom: 2,
-                style: allsubmittedStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: false
-        },
-
-        {
-            key: 'Developable sites SHLAA 2020',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_sites&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: developablesitesPopup,
-                maxZoom: 2,
-                style: developablesitesStyle
-
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'Sites that did not pass first sift of SHLAA 2020',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_not_assessed&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: notassessedPopup,
-                maxZoom: 2,
-                style: notassessedStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: false
         },
 
         {
@@ -132,6 +42,7 @@ const Configuration = {
             displayOverlay: false,
             visibleByDefault: true
         },
+        
         {
             key: 'os1250_text',
             url: 'http://spatial.stockport.gov.uk/geoserver/wms?',
--- src/Tiles/index.js
+++ src/Tiles/index.js
@@ -2,7 +2,7 @@ import Leaflet from 'leaflet'
 import mapboxGL from 'mapbox-gl-leaflet' // eslint-disable-line no-unused-vars
 
 const token =
-  'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJjazV4dWRuNm4wNHMyM2pwM3l5M24zZjUwIn0.2jJpnKTnqeBcZZosxhu7hw'
+  'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja2s3MHdjb2gwOWpkMzJxd283ZW9ia210In0.qZNmQAJyGKtYYUxl3DOo6w'
 
 const greyscale = Leaflet.mapboxGL({
   style: 'mapbox://styles/gis-stockport/ck5gr2oav0utc1ipbdkcjnjop',
